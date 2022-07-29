f73b04e497ab511b259b2431dd3f78c88399cf84
Marcio Bueno <mabuen@live.com>
2022-07-30 01:54:58 +0200

    aula 11

---
+++ aula11/ex08.js
@@ -0,0 +1,6 @@
+var vel=100
+console.log(`A sua velocidade é ${vel}Km/h`)
+if(vel>60){ //condicäo simples
+    console.log("Ultrapassou a velocidade parmitida. MULTADO!")
+}
+console.log("Use sempre o cinto de seguranca!")
