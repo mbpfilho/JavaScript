c6a799bfde29afa8a489afdb1caf28a2e6bd7bc5
Marcio Bueno <mabuen@live.com>
2022-07-30 17:45:58 +0200

    aula 12

---
+++ aula12/ex11.js
@@ -0,0 +1,9 @@
+var idade=17
+console.log(`${idade} anos`)
+if(idade<16){
+    console.log("Näo vota")
+}else if(idade<18||idade>65){
+        console.log("Voto opcional")
+}else{
+    console.log("Voto obrigatório")
+}
---
+++ aula12/ex13.js
@@ -0,0 +1,37 @@
+var agora=new Date()
+var diaSem=agora.getDay()
+/*
+0 domingo
+1 segunda
+2 terca
+3 quarta 
+4 quinta 
+5 sexta 
+6 sábado
+*/
+switch(diaSem){
+    case 0:
+        console.log("domingo")
+        break
+    case 1:
+        console.log("segunda")
+        break
+    case 2:
+        console.log("terca")
+        break
+    case 3:
+        console.log("quarta")
+        break
+    case 4:
+        console.log("quinta")
+        break 
+    case 5:
+        console.log("sexta")
+        break 
+    case 6:
+        console.log("sábado")
+        break
+    default:
+        console.log("ERRO: dia inválido")
+        break
+}
