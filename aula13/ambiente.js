8c0a2df56959e915a2c2986a170bc9b1da4ff6c5
Marcio Bueno <mabuen@live.com>
2022-07-31 20:20:46 +0200

    aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c=0
+do{
+    c++
+    console.log(`passo ${c}`)
+}while(c<6)
+
+/*
+var c=0
+while(c<6){
+    c++
+    console.log(`passo ${c}`)
+}
+*/
+
+/*
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+*/
